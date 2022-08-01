48e629a9a84ae5dca47c3b43cd24f6bc5d721c5e
carlfombuena <53881761+carlfombuena@users.noreply.github.com>
2022-08-01 12:48:17 -0400

    Complete Dice Game Demo Kotlin version 0

--- app/src/main/java/com/example/rollthedice/MainActivity.kt
+++ app/src/main/java/com/example/rollthedice/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.rollthedice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+
+
+        rollButton.setOnClickListener {
+            Toast.makeText(this,"Dice Rolled", Toast.LENGTH_SHORT).show()
+
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val dice=Dice(6)
+        val diceRoll=dice.roll()
+        val showtext: TextView=findViewById(R.id.textView)
+        showtext.text=diceRoll.toString()
     }
 }
