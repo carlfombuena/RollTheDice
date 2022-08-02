88efad9f77b85de7d24e68aca00003005d5f3381
carlfombuena <53881761+carlfombuena@users.noreply.github.com>
2022-08-02 15:01:16 -0400

    Complete Dice Game Demo Kotlin version 1

--- app/src/main/java/com/example/rollthedice/MainActivity.kt
+++ app/src/main/java/com/example/rollthedice/MainActivity.kt
@@ -8,22 +8,46 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-         super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
 
-
+// this part is when you click the button triggers the function
         rollButton.setOnClickListener {
-            Toast.makeText(this,"Dice Rolled", Toast.LENGTH_SHORT).show()
-
+            Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT).show()
             rollDice()
         }
     }
-
+//This part controll the dice function
     private fun rollDice() {
-        val dice=Dice(6)
-        val diceRoll=dice.roll()
-        val showtext: TextView=findViewById(R.id.textView)
-        showtext.text=diceRoll.toString()
+    //Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+    //Update the textview element with the dice roll
+    val showtext: TextView = findViewById(R.id.textView)
+    //Conditional Statement to alter textview element to show typeface dice
+    if(diceRoll==1){
+        showtext.text = "⚀"
+    }else if (diceRoll==2)
+    {
+        showtext.text = "⚁"
+    }
+    else if (diceRoll==3)
+    {
+        showtext.text = "⚂"
+    }
+    else if (diceRoll==4)
+    {
+        showtext.text = "⚃"
+    }
+    else if (diceRoll==5)
+    {
+        showtext.text = "⚄"
+    }
+    else
+    {
+        showtext.text = "⚅"
+    }
+
     }
 }
