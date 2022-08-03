6eaa2b5c9a9a0af9550aada956136365a0b2fd58
carlfombuena <53881761+carlfombuena@users.noreply.github.com>
2022-08-03 10:44:41 -0400

    Complete Dice Game Demo Kotlin Version 1.5 made a few changes delete some redundant value

--- app/src/main/java/com/example/rollthedice/MainActivity.kt
+++ app/src/main/java/com/example/rollthedice/MainActivity.kt
@@ -20,19 +20,15 @@ class MainActivity : AppCompatActivity() {
     }
 //This part controll the dice function
     private fun rollDice() {
-    //Create new Dice object with 6 sides and roll it
-        val dice = Dice(6)
-    val secondDice=Dice(6)
-        val diceRoll = dice.roll()
-        val secondDiceRoll=secondDice.roll()
+    //Create new Dice Array object with 6 sides and roll it
         val diceArray= arrayOf("⚀","⚁","⚂","⚃","⚄","⚅")
     //Tracking the textview element with the dice roll
     val showtext: TextView = findViewById(R.id.textView)
     val showtext2: TextView = findViewById(R.id.textView2)
     //Using an Array to contain dice typetext and then index it using rolled number
-    //Array starts to one so applied a negative 1 to reach 0 index and 5th index
-    showtext.text=diceArray[diceRoll-1]
-    showtext2.text=diceArray[secondDiceRoll-1]
+    //Array starts to zero so applied a negative 1 to reach 0 index and 5th index
+    showtext.text=diceArray[Dice(6).roll()-1]
+    showtext2.text=diceArray[Dice(6).roll()-1]
 
 
 
