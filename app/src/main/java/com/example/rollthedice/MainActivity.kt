bd5525376c3d89572de3cff49e7bee6d4a529319
carlfombuena <53881761+carlfombuena@users.noreply.github.com>
2022-08-03 12:59:40 -0400

    Complete Dice Game Demo Kotlin Version 2.0 Replace the array of dice typetext to dice Image

--- app/src/main/java/com/example/rollthedice/MainActivity.kt
+++ app/src/main/java/com/example/rollthedice/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.rollthedice
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,18 +20,33 @@ class MainActivity : AppCompatActivity() {
             rollDice()
         }
     }
-//This part controll the dice function
-    private fun rollDice() {
-    //Create new Dice Array object with 6 sides and roll it
-        val diceArray= arrayOf("⚀","⚁","⚂","⚃","⚄","⚅")
-    //Tracking the textview element with the dice roll
-    val showtext: TextView = findViewById(R.id.textView)
-    val showtext2: TextView = findViewById(R.id.textView2)
-    //Using an Array to contain dice typetext and then index it using rolled number
-    //Array starts to zero so applied a negative 1 to reach 0 index and 5th index
-    showtext.text=diceArray[Dice(6).roll()-1]
-    showtext2.text=diceArray[Dice(6).roll()-1]
 
+    //This part controll the dice function
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun rollDice() {
+        //Create new Dice Array object with 6 sides and roll it
+//        val diceArray = arrayOf("⚀", "⚁", "⚂", "⚃", "⚄", "⚅")
+        //Tracking the textview element with the dice roll
+//        val showtext: TextView = findViewById(R.id.textView)
+//        val showtext2: TextView = findViewById(R.id.textView2)
+        val showImage: ImageView = findViewById(R.id.imageView)
+        val showSecondImage:ImageView=findViewById(R.id.imageView2)
+        //Using an Array to contain dice typetext and then index it using rolled number
+        //Array starts to zero so applied a negative 1 to reach 0 index and 5th index
+//        showtext.text = diceArray[Dice(6).roll() - 1]
+//        showtext2.text = diceArray[Dice(6).roll() - 1]
+        // Create an array of image id or path location
+        val diceImageArray = arrayOf(
+            ResourcesCompat.getDrawable(resources,R.drawable.dice_1,null),
+            ResourcesCompat.getDrawable(resources,R.drawable.dice_2,null),
+            ResourcesCompat.getDrawable(resources,R.drawable.dice_3,null),
+            ResourcesCompat.getDrawable(resources,R.drawable.dice_4,null),
+            ResourcesCompat.getDrawable(resources,R.drawable.dice_5,null),
+            ResourcesCompat.getDrawable(resources,R.drawable.dice_6,null),
+        )
+        //Using an Array image shows and set by index rolled by the dice
+        showImage.setImageDrawable(diceImageArray[Dice(6).roll()-1])
+        showSecondImage.setImageDrawable(diceImageArray[Dice(6).roll()-1])
 
 
     }
